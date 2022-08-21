d4773a9abb2c6d8d9f335c4984d7d1ef9c9ce5d3
s13g <ayflix0@gmail.com>
2022-08-21 01:14:48 +0100

    main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   window.addEventListener("scroll", function () {
-//     if (window.scrollY > 100) {
-//       let element = document.getElementById("pages");
-//       element.classList.add("pages2")
-//     } else {
-//       let element = document.getElementById("pages")
-//       element.classList.remove(".pages2")
-//     }
-//   })
-// })
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 214) {
+      let element = document.querySelector(".navbar");
+      element.classList.add("pages2")
+      element.classList.remove("pages")
+    } else {
+      let element = document.querySelector(".navbar");
+      element.classList.remove("pages2")
+      element.classList.add("pages")
+    }
+  })
+})
