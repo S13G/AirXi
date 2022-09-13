e8cadee5c3933ff03ee08d2b35204c6fb9f8174b
S13G <ayflix0@gmail.com>
2022-09-13 16:12:55 +0100

    fixed css

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 214) {
-      let element = document.querySelector(".navbar");
-      element.classList.add("pages2")
-      element.classList.remove("pages")
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 214) {
+            let element = document.querySelector(".navbar");
+            element.classList.add("pages2")
+            element.classList.remove("pages")
 
-    } else {
-      let element = document.querySelector(".navbar");
-      element.classList.remove("pages2")
-      element.classList.add("pages")
-    }
-  })
+        } else {
+            let element = document.querySelector(".navbar");
+            element.classList.remove("pages2")
+            element.classList.add("pages")
+        }
+    })
 })
